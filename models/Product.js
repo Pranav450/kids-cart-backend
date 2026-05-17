09e8e098da313458a217fe5ec85d2ea04b868a03
Pranav450 <65111449+Pranav450@users.noreply.github.com>
2026-05-17 19:22:23 +0530

    Update Product

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  item: String,
-  name: String,
-  price: Number,
-  stock: Number,
-  image: String
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    stock: {
+        type: Number,
+        default: 100
+    }
 });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model("Product", ProductSchema);
