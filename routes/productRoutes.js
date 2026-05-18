7c82042e8972877ac79e9d082ea2690a5865f4a1
Pranav450 <65111449+Pranav450@users.noreply.github.com>
2026-05-18 18:01:20 +0530

    Update productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,38 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 
-router.get("/products", async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
-});
-
-router.get("/product/:item", async (req, res) => {
-  const product = await Product.findOne({
-    item: req.params.item
-  });
-
-  res.json(product);
-});
-
-router.post("/order", async (req, res) => {
-  const { item } = req.body;
-
-  const product = await Product.findOne({ item });
-
-  if (!product || product.stock <= 0) {
-    return res.status(400).json({
-      message: "Out of stock"
-    });
+// GET all products
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-
-  product.stock -= 1;
-
-  await product.save();
-
-  res.json({
-    success: true,
-    stock: product.stock
-  });
 });
 
 module.exports = router;
